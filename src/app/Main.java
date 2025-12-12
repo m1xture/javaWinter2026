fa74f70fcc55e361a2df50d612f4298bd5293a80
m1xture <misha.ivanov.1050@gmail.com>
2025-12-12 21:52:14 +0200

    add kgs converter

---
+++ src/app/Main.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Main {
+    private static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter app");
+
+        double kgs = 5;
+        double pounds = convertKgsToPounds(kgs);
+        System.out.printf("Result is %.2f pounds", pounds);
+    }
+
+    private static double convertKgsToPounds(double kgs) {
+        return kgs * CONV_K;
+    }
+}
